121b4de2cc8111fc126134270f90e0b4d49f8d4a
Jannes Behrens <jannescb@me.com>
2021-12-17 11:16:50 +0100

    wip

--- src/Console/PageBuilderCommand.php
+++ src/Console/PageBuilderCommand.php
@@ -24,7 +24,7 @@ class PageBuilderCommand extends BaseCommand
      * @var array
      */
     public $composerPackages = [
-        'aw-studio/laravel-page-builder',
+        'aw-studio/laravel-sites',
     ];
 
     /**
